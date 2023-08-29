813f04657310fbfdb9c551df5396765d9e0f3163
M Sajid <msajidnaizi@gmail.com>
2023-08-29 07:49:06 +0500

    initial commit

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './navbar.css'
+import logo from '../../assets/logo.png'
+import contactImg from '../../assets/contact.png'
+import {Link} from 'react-scroll'
+
+const Navbar = () => {
+  return (
+    <div className='navcontainer'>
+    <nav className='navbar'>
+        <img className='logo' src={logo} />
+        <div className='menu-items'>
+        <Link className='item'>Home</Link>
+        <Link className='item'>About</Link>
+        <Link className='item'>Porfolio</Link>
+        <Link className='item'>Clients</Link>
+        </div>
+        <button className='menu-btn'>
+            <img src={contactImg} className='contactImg'/>Contact Me </button>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
